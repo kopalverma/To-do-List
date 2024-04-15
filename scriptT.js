ea9a85f4758bda852adf96ca4e40a2ec3846a532
Kopal Verma <163647743+kopalverma@users.noreply.github.com>
2024-04-15 21:32:49 +0530

    Update scriptT.js

--- scriptT.js
+++ scriptT.js
@@ -1,23 +1,21 @@
 let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
-let li1 = document.querySelectorAll("li");
+
 btn.addEventListener("click", function() {
     let input = document.querySelector("input");
     let li  = document.createElement("li");
     let delBtn  = document.createElement("button");
-    delBtn.classList.add( "delete" );
-    delBtn.innerText="delete";
-    li.innerText=input.value;
-    console.log(input.value);
-    input.value="";
+    delBtn.classList.add("delete");
+    delBtn.innerText = "delete";
+    li.innerText = input.value;
+    input.value = "";
     ul.appendChild(li);
     li.appendChild(delBtn);
-}
-);
-let delBtns = document.querySelectorAll(".delete");
-for(delBtn of delBtns)
-{
-    delBtn.addEventListener("click",function(){
-        this.parentElement.remove();
-})
-}
+});
+
+// Delegate the event handling to the parent element 'ul'
+ul.addEventListener("click", function(event) {
+    if (event.target.classList.contains("delete")) {
+        event.target.parentElement.remove();
+    }
+});
